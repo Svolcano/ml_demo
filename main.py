965ff6c4b81268aae8c877ca970710420ebc002c
scw <scw>
2022-09-22 23:10:47 +0800

    optimize epoch

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from collections import Counter
 import random
 import matplotlib.pyplot as plt
 from datetime import datetime
+import pickle
 
 
 step = 2
@@ -93,7 +94,7 @@ def read_data_from_file(file_path):
                     target_data = t_target_data
                 else:
                     t = 100
-                    if t > fl :
+                    if t > fl:
                         t = fl
                     target_data = random.sample(f_target_data, t)
             return wash_data(target_data)
@@ -101,6 +102,7 @@ def read_data_from_file(file_path):
         print(e)
         print(file_path)
     return None, None
+
 
 @time_it
 def load_data(root_path="D:\小任务数据\export"):
@@ -119,24 +121,26 @@ def load_data(root_path="D:\小任务数据\export"):
             if data is not None:
                 total_data.append(data)
                 total_label.append(label)
-                
-    train(total_data, total_label)
+
+    save_washed_data(total_data, total_label)
+    return total_data, total_label
+
 
 def wash_data(total_datas):
     df = pd.DataFrame(total_datas)
     data = df[range(11)]
-    
+
     label = df[11]
     data = data.apply(lambda x: [i[1] for i in x])
-    
+
     data = data.to_numpy()
     label = label.to_numpy()
-    data = data.astype('float32')
-    label = label.astype('float32')
+    data = data.astype("float32")
+    label = label.astype("float32")
     mm = data.max()
     mmin = data.min()
     data -= mmin
-    std =  mm - mmin
+    std = mm - mmin
     try:
         data /= std
     except Exception as e:
@@ -156,67 +160,99 @@ def build_model(shape):
     model.add(layers.Dense(64, activation=activations.relu))
     # model.add(layers.Dense(64, activation=activations.relu))
     model.add(layers.Dense(1, activation=activations.sigmoid))
-    model.compile(
-        optimizer="adam", loss=losses.binary_crossentropy, metrics=['acc']
-    )
+    model.compile(optimizer="adam", loss=losses.binary_crossentropy, metrics=["acc"])
     # metric accuracy : dict_keys(['loss', 'accuracy', 'val_loss', 'val_accuracy'])
     # metric acc : dict_keys(['loss', 'acc', 'val_loss', 'val_acc'])
     return model
+
 
 def paint(history):
     plt.subplot(1, 2, 1)
     history_dict = history.history
     print(history_dict.keys())
-    loss_values = history_dict['loss']
-    val_loss = history_dict['val_loss']
+    loss_values = history_dict["loss"]
+    val_loss = history_dict["val_loss"]
     epochs = range(1, len(loss_values) + 1)
-    plt.plot(epochs, loss_values, 'bo', label='Training loss')
-    plt.plot(epochs, val_loss, 'b', label='Testing loss')
-    plt.title('Training vs Test loss')
-    plt.xlabel('Epochs')
-    plt.ylabel('Loss')
+    plt.plot(epochs, loss_values, "bo", label="Training loss")
+    plt.plot(epochs, val_loss, "b", label="Testing loss")
+    plt.title("Training vs Test loss")
+    plt.xlabel("Epochs")
+    plt.ylabel("Loss")
     plt.legend()
 
-
-    plt.subplot(1, 2, 2 )
-    acc = history_dict['acc']
-    val_acc = history_dict['val_acc']
+    plt.subplot(1, 2, 2)
+    acc = history_dict["acc"]
+    val_acc = history_dict["val_acc"]
     epochs = range(1, len(acc) + 1)
-    plt.plot(epochs, acc, 'bo', label='Training acc')
-    plt.plot(epochs, val_acc, 'b', label='Testing acc')
-    plt.title('Training vs Test accuracy')
-    plt.xlabel('Epochs')
-    plt.ylabel('Acc')
+    plt.plot(epochs, acc, "bo", label="Training acc")
+    plt.plot(epochs, val_acc, "b", label="Testing acc")
+    plt.title("Training vs Test accuracy")
+    plt.xlabel("Epochs")
+    plt.ylabel("Acc")
     plt.legend()
     plt.show()
 
-def train(total_data, total_label):
+
+def save_washed_data(total_data, total_label):
+    data = {"data": total_data, "label": total_label}
+    with open("./ml_demo/washed_data.pkl", "wb") as wh:
+        pickle.dump(data, wh)
+
+
+def load_washed_data(data_path="./ml_demo/washed_data.pkl"):
+    # data = {
+    #     'data': total_data,
+    #     'label':total_label
+    # }
+    with open("./ml_demo/washed_data.pkl", "rb") as rh:
+        data = pickle.load(rh)
+        return data["data"], data["label"]
+
+
+def train():
+    data_path = "./ml_demo/washed_data.pkl"
+    if os.path.exists(data_path):
+        print("load data from pickle")
+        total_data, total_label = load_washed_data(data_path)
+    else:
+        print("load data from csv")
+        total_data, total_label = load_data()
     model_name = "ml_model_{}_v{}.h5".format(datetime.now().strftime("%Y_%m_%d"), 1)
     data = np.vstack(total_data)
     label = np.hstack(total_label)
-    label = label.astype('float32')
+    label = label.astype("float32")
     data_len = data.shape[0]
     train_size = int(data_len * 0.8)
     test_size = data_len - train_size
     val_size = int(train_size * 0.2)
     train_size = train_size - val_size
     split_size = [train_size, test_size, val_size]
-    print(split_size)
+    # print(split_size)
     train_data, test_data, val_data = tf.split(data, split_size, axis=0)
-    print(train_data.shape, test_data.shape, val_data.shape)
+    # print(train_data.shape, test_data.shape, val_data.shape)
     train_label, test_label, val_label = tf.split(label, split_size, axis=0)
-    print(Counter(train_label.numpy()), Counter(test_label.numpy()), Counter(val_label.numpy()))
+    # print(
+    #     Counter(train_label.numpy()),
+    #     Counter(test_label.numpy()),
+    #     Counter(val_label.numpy()),
+    # )
     model = build_model(train_data.shape[1])
-    history = model.fit(train_data, train_label, epochs=40, batch_size=64, validation_data=(test_data, test_label))
+    history = model.fit(
+        train_data,
+        train_label,
+        epochs=11,
+        batch_size=64,
+        validation_data=(test_data, test_label),
+    )
     paint(history)
     model.save(model_name)
 
     del model
     loaded_model = models.load_model(model_name)
     print(loaded_model.summary())
-    loss,acc = loaded_model.evaluate(val_data, val_label, verbose=2)
+    loss, acc = loaded_model.evaluate(val_data, val_label, verbose=2)
     print(loss, acc)
 
 
 if __name__ == "__main__":
-    load_data()
+    train()
